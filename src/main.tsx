fd7617e405b44bb4efc56deb718346be57f398c7
Ajitesh Chhatani <ajiteshchhatani@gmail.com>
2023-08-10 17:25:55 +0400

    Adds basename option to routes for deployment

--- src/main.tsx
+++ src/main.tsx
@@ -6,22 +6,27 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import AccordionHome from "./components/Accordion/index.tsx";
 import StarRating from "./components/Star_Rating/index.tsx";
 
-const routes = createBrowserRouter([
+const routes = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          index: true,
+          element: <AccordionHome />,
+        },
+        {
+          path: "star_rating",
+          element: <StarRating />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        index: true,
-        element: <AccordionHome />,
-      },
-      {
-        path: "star_rating",
-        element: <StarRating />,
-      },
-    ],
-  },
-]);
+    basename: import.meta.env.DEV ? "/" : "/react-components/",
+  }
+);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
